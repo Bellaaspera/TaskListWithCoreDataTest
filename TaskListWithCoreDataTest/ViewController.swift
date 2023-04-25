c7a52b0c202438e052ae9bdd11b91b417e873f7a
Bellaaspera <lanasenatorova@gmail.com>
2023-04-25 21:08:31 +0300

    Initial Commit

---
+++ TaskListWithCoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListWithCoreDataTest
+//
+//  Created by Светлана Сенаторова on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
